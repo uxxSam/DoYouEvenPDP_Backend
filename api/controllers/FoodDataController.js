561ad5388d40b1b942284e82c83bda8e1baa6c64
Bicheng Xu <xbc010@gmail.com>
2017-11-04 18:18:51 -0700

    c

--- api/controllers/FoodDataController.js
+++ api/controllers/FoodDataController.js
@@ -7,17 +7,6 @@
 
 module.exports = {
 	fetch: function(req, res) {
-		if (!req.headers.id) return res.send(403, {
-			message: 'Please Login first',
-			Status: -1
-		});
-		User.find({ id: req.headers.id }).exec(function (err, usersNamedFinn){
-		  if (err || usersNamedFinn.length == 0) {
-		    return res.send(403, {
-					message: 'Wrong Login',
-					Status: -1
-				})
-		  } else {
 					FoodData.find().exec(function (err, foodLocation){
 						if (err || foodLocation.length == 0) {
 							return res.send(403, {
@@ -31,6 +20,4 @@ module.exports = {
 						}
 					})
 				}
-			})
-		}
-	};
+			};
